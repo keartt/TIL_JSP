308cc1c35ba961aeac7ac86c149ba0be769f4efb
keartt <shalpha_2@naver.com>
2021-05-08 23:53:45 +0900

    data base

---
+++ BBS/src/main/java/user/User.java
@@ -0,0 +1,42 @@
+package user;
+
+public class User {
+
+		private String userID;
+		private String userpassword;
+		private String userName;
+		private String userGender;
+		private String userEmail;
+		public String getUserID() {
+			return userID;
+		}
+		public void setUserID(String userID) {
+			this.userID = userID;
+		}
+		public String getUserpassword() {
+			return userpassword;
+		}
+		public void setUserpassword(String userpassword) {
+			this.userpassword = userpassword;
+		}
+		public String getUserName() {
+			return userName;
+		}
+		public void setUserName(String userName) {
+			this.userName = userName;
+		}
+		public String getUserGender() {
+			return userGender;
+		}
+		public void setUserGender(String userGender) {
+			this.userGender = userGender;
+		}
+		public String getUserEmail() {
+			return userEmail;
+		}
+		public void setUserEmail(String userEmail) {
+			this.userEmail = userEmail;
+		}
+	
+		
+}
